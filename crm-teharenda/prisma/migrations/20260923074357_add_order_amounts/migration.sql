430c1601d262de8e92e041a45fdd79ef5f30511c
Михаил <teharenda1989@gmail.com>
2026-09-24 21:37:52 +0700

    Delete crm-teharenda directory

--- crm-teharenda/prisma/migrations/20260923074357_add_order_amounts/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN "closedAt" DATETIME;
-ALTER TABLE "Order" ADD COLUMN "commissionAmount" INTEGER;
-ALTER TABLE "Order" ADD COLUMN "orderAmount" INTEGER;
-ALTER TABLE "Order" ADD COLUMN "result" TEXT;
